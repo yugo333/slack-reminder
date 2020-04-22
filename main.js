7bba2384ea3db1c22be3058be53098f2db5f2ce0
Tatsuya <tatsuya.asami@asia-quest.jp>
2020-04-22 13:46:25 +0900

    リマインドの日時の選択で表示させるinput要素を切り替え出来る機能を実装

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
 
   const messageSender = document.getElementById("messageSender");
   messageSender.addEventListener(
-    "keyup",
+    "change",
     () => {
       const { value } = messageSender;
       if (value.match(/@/) || value.match(/#/)) {
@@ -30,7 +30,7 @@
 
   const messageContents = document.getElementById("messageContents");
   messageContents.addEventListener(
-    "keyup",
+    "change",
     () => {
       const { value } = messageContents;
       createdMessageValues.contents = `"${value}"`;
@@ -42,7 +42,75 @@
   remindType.addEventListener("change", () => {
     const { value } = remindType.type;
     createdMessageValues.remindType = value;
+
+    if (value === "in") {
+      createInSelectBox();
+      return;
+    }
+    if (value === "on") {
+      createOnSelectBox();
+    }
   });
+
+  const wrapper = document.getElementById("wrapper");
+
+  const createInSelectBox = () => {
+    // すでに日時指定の要素を作っている場合は削除
+    const onForm = document.getElementById("onForm");
+    if (onForm) {
+      onForm.remove();
+    }
+
+    const inFormElement = document.createElement("form");
+    inFormElement.id = "inForm";
+    wrapper.appendChild(inFormElement);
+
+    const inputNumber = document.createElement("input");
+    inputNumber.type = "number";
+    // クリックでは5単位で変わる。それより小さい単位は手入力出来る。
+    inputNumber.step = 5;
+    inputNumber.min = 0;
+    inputNumber.placeholder = "数字を入力";
+    inFormElement.appendChild(inputNumber);
+
+    const selectbox = document.createElement("select");
+    selectbox.id = "selectTime";
+    inFormElement.appendChild(selectbox);
+
+    // TODO: minutesとhoursであっているか確認。
+    const options = [
+      { text: "分後", value: "minutes" },
+      { text: "時間後", value: "hours" },
+    ];
+
+    options.forEach((object) => {
+      const optionElement = document.createElement("option");
+      optionElement.text = object.text;
+      optionElement.value = object.value;
+      selectbox.appendChild(optionElement);
+    });
+  };
+
+  const createOnSelectBox = () => {
+    const inForm = document.getElementById("inForm");
+    if (inForm) {
+      inForm.remove();
+    }
+    // 日付のセレクトボックスと時間のセレクトボックスを作成
+
+    // 日時指定で使用するelement
+    const onFormElement = document.createElement("form");
+    onFormElement.id = "onForm";
+    wrapper.appendChild(onFormElement);
+
+    const inputDate = document.createElement("input");
+    inputDate.type = "date";
+    onFormElement.appendChild(inputDate);
+
+    const inputTime = document.createElement("input");
+    inputTime.type = "time";
+    onFormElement.appendChild(inputTime);
+  };
 
   copy.addEventListener("click", () => {
     out.select();
